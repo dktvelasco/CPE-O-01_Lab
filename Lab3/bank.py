d7d8f9a4825372fe5434ad8aa321d378a318304e
DaveBSTA <127595500+DaveBSTA@users.noreply.github.com>
2023-03-19 19:43:36 +0800

    Update bank.py

--- Lab3/bank.py
+++ Lab3/bank.py
@@ -34,7 +34,9 @@ class Bank:
 
     def __str__(self):
         """Returns the string representation of the bank."""
-        return "\n".join([str(v) for (k, v) in sorted(self.accounts.items(),
+        return "\n".join([str(v) for (k, v) 
+                    in sorted(self.accounts.items(),
+                           key=lambda cv: cv[1].getName())])
 
     def makeKey(self, name, pin):
         """Returns a key for the account."""
