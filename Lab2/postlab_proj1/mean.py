68c671188d1f60dc9e4d8c25a0a20e84158df2c6
Janarie Ulysses B. Tatad <125622924+ulysheesh@users.noreply.github.com>
2023-03-22 18:00:17 +0800

    Update of mean
    Here's the update of the mean code

--- Lab2/postlab_proj1/mean.py
+++ Lab2/postlab_proj1/mean.py
@@ -3,23 +3,11 @@ File: mean.py
 Prints the mean of a set of numbers in a file.
 """
 
-fileName = input("Enter the file name: ")
-f = open(fileName, 'r')
-
-# Input the text, convert it to numbers, and
-# add the numbers to a list
-numbers = []
-for line in f:
-    words = line.split()
-    for word in words:
-        numbers.append(float(word))
-
-# Sort the list and print the sum of all numbers divided by its length
-numbers.sort()
-mean = sum(numbers) / len(numbers)
-print("\nThe mean is", end=" ",)
-if len(numbers) == 0:
-    print(0)
-else:
-    print (sum(numbers) / len(numbers))
-
+file_name = input("Enter the name of the file containing the numbers: ")
+with open(file_name, 'r')as file:
+    numbers = [float(line.strip())for line in file]
+    if len(numbers) == 0:
+        mean = 0
+    else:
+        mean = sum(numbers) / len(numbers)
+        print (f"The mean of the numbers in {file_name} is {mean:.2f}.")
