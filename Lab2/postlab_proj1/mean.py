1db0fca0a111a27e85ef9b30123f36c6644eee4b
dktvelasco <125556537+dktvelasco@users.noreply.github.com>
2023-03-22 23:56:32 +0800

    simplified and improved
    Simplified the code and made it easier to integrate into stats.py

--- Lab2/postlab_proj1/mean.py
+++ Lab2/postlab_proj1/mean.py
@@ -3,11 +3,8 @@ File: mean.py
 Prints the mean of a set of numbers in a file.
 """
 
-file_name = input("Enter the name of the file containing the numbers: ")
-with open(file_name, 'r')as file:
-    numbers = [float(line.strip())for line in file]
+def mean(numbers):
     if len(numbers) == 0:
-        mean = 0
-    else:
-        mean = sum(numbers) / len(numbers)
-        print (f"The mean of the numbers in {file_name} is {mean:.2f}.")
+        return 0
+    
+    return sum(numbers) / len(numbers)
