665a8a01d9bf432e99a59067b5c77c293dcc8901
Jose Carlos Miguel R. Aspril <125950999+JoseAspril@users.noreply.github.com>
2023-03-23 20:25:27 +0800

    Update LR2_2.py

--- Lab2/postlab_proj2/LR2_2.py
+++ Lab2/postlab_proj2/LR2_2.py
@@ -6,3 +6,12 @@ except FileNotFoundError:
     print(f"File {file_name} not found.")
     exit()
 num_lines = len(lines)
+
+print(f"The file {file_name} contains {num_lines} lines.")
+while True:
+    line_num = input(f"Enter a line number (1 to {num_lines}, or 0 to quit): ")
+
+    if line_num == '0':
+        break
+    try:
+        line_num = int(line_num)
