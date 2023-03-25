b9205ff4426b63c973b1533830e547480e7d92cd
Jose Carlos Miguel R. Aspril <125950999+JoseAspril@users.noreply.github.com>
2023-03-25 22:50:18 +0800

    Update test3.py

--- Lab4/postlab_proj3/test3.py
+++ Lab4/postlab_proj3/test3.py
@@ -11,3 +11,25 @@ class TestOXOArgsUI(unittest.TestCase):
                 oxo_args_ui.playGame(game)
                 mock_save.assert_called_once_with(game)
                 mock_exit.assert_called_once()
+    
+    
+    @patch('builtins.input', side_effect=['invalid', '1', 'invalid', '2', 'q'])
+    def test_playGame_invalid_input(self, mock_input):
+        game = [[None]*3 for _ in range(3)]
+        with patch('oxo_logic.userMove') as mock_user:
+            with patch('oxo_logic.computerMove') as mock_computer:
+                mock_user.side_effect = ['invalid', None, None, 'X']
+                oxo_args_ui.playGame(game)
+                mock_user.assert_has_calls([call(game, 0), call(game, 1)])
+                mock_computer.assert_called_once_with(game)
+    @patch('builtins.input', side_effect=['q'])
+    def test_playGame_draw(self, mock_input):
+        game = [['O', 'X', 'O'], ['X', 'X', 'O'], ['O', 'O', 'X']]
+        with patch('oxo_logic.userMove') as mock_user:
+            with patch('oxo_logic.computerMove') as mock_computer:
+                result = oxo_args_ui.playGame(game)
+                mock_user.assert_not_called()
+                mock_computer.assert_not_called()
+                self.assertEqual(result, 'D')
+    
+    
