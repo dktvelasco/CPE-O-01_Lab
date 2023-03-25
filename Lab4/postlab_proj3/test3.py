b1c507ab810542db2dbde7c466e87b1d177c1493
Clark_Felix <127593258+sirflakes@users.noreply.github.com>
2023-03-25 22:43:43 +0800

    Update test3.py

--- Lab4/postlab_proj3/test3.py
+++ Lab4/postlab_proj3/test3.py
@@ -1,1 +1,13 @@
-# Create a unit test program for testing  the Tic Tac Toe Console App
+import unittest
+from unittest.mock import patch
+import io
+import oxo_args_ui
+class TestOXOArgsUI(unittest.TestCase):
+    @patch('builtins.input', side_effect=['1', 'q'])
+    def test_playGame_quit(self, mock_input):
+        game = [[None]*3 for _ in range(3)]
+        with patch('oxo_logic.saveGame') as mock_save:
+            with patch('sys.exit') as mock_exit:
+                oxo_args_ui.playGame(game)
+                mock_save.assert_called_once_with(game)
+                mock_exit.assert_called_once()
